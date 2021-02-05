cb1d7bf68012c6c40ee84b7e59d5983978e7e235
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-02-05 00:15:31 -0700

    acmeAccounting created

---
+++ acmeAccountingHelloWorld/acmeAccountingHelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace acmeAccountingHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\nWelcome to Acme Accounting Systems! \nRemember, we're \"accounting\" on you! \n\n\n\n\n...Yeah, we'll see ourselves out.");
+            Console.Read();
+        }
+    }
+}
