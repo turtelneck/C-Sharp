1eeb08086f1a35244d4b87a3d29d50b06dfd7a70
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-02-08 12:03:31 -0700

    Create basic_do_while_app

---
+++ basic_do_while_app/basic_do_while_app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace basic_do_while_app
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isTrue = true;
+
+            while (isTrue)
+            {
+                Console.WriteLine("\n\nHello!");
+                isTrue = false;
+            }
+            do
+            {
+                Console.WriteLine("\nHello again!");
+            } while (isTrue);
+
+            Console.Read();
+        }
+    }
+}
