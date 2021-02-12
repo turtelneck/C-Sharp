29cd4f27a58aab97a6c3b4fea4e3862ffa4dbe50
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-02-12 09:28:42 -0700

    create basic_class_app3

---
+++ basic_class_app3/basic_class_app3/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basic_class_app3
+{
+    class Class1
+    {
+        public int Method1(int num1, int num2 = 10)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ basic_class_app3/basic_class_app3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace basic_class_app3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("\nHey! Enter any whole number.");
+            int userPick1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nNow enter another whole number or press enter and we'll make it 10 by default.");
+            string userPick2 = Console.ReadLine();
+
+            Class1 inst = new Class1();
+
+            if (userPick2 == "")
+            {
+                Console.WriteLine("\n{0} + 10 = {1}", userPick1, inst.Method1(userPick1));
+            }
+            else
+            {
+                Console.WriteLine("\n{0} + {1} = {2}", userPick1, userPick2, inst.Method1(userPick1, Convert.ToInt32(userPick2)));
+            }
+
+            Console.Read();
+        }
+    }
+}
