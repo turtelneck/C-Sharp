5d62be861df4df399884ae17320d598cd4933008
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-02-12 09:40:04 -0700

    create basic_class_app4

---
+++ basic_class_app4/basic_class_app4/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basic_class_app4
+{
+    class Class1
+    {
+        public void Method1(int num1, int num2)
+        {
+            Console.WriteLine("\n{0} * 2 = {1}", num1, num1 * 2);
+            Console.WriteLine("num2 is {0}", num2);
+        }
+    }
+}
---
+++ basic_class_app4/basic_class_app4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace basic_class_app4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 inst = new Class1();
+
+            inst.Method1(100, 10);
+            inst.Method1(num2: 10, num1: 100);
+
+            Console.Read();
+        }
+    }
+}
