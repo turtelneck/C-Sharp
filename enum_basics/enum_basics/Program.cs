04a882f384205a8350b094215f6a1b140b320066
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-02-18 17:03:00 -0700

    create enum_basics

---
+++ enum_basics/enum_basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace enum_basics
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("\nPlease enter the current day of the week.");
+            string UserPick = Console.ReadLine();
+            try
+            {
+                // checks to ensure UserPick is a value in DaysOfTheWeek
+                Enum.Parse(typeof(DaysOfTheWeek), UserPick, true);
+            }
+            catch (ArgumentException)
+            {
+                // if UserPick is not a value in DaysOfTheWeek, error is thrown
+                Console.WriteLine("\nPlease print an actual day of the week.");
+                throw;
+            }
+        }
+    }
+}
