fb6b242697bfa944d777469ef804d660a01748a8
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-02-22 21:13:02 -0700

    create const_var_constructorchain

---
+++ const_var_constructorchain/const_var_constructorchain/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace const_var_constructorchain
+{
+    class Animal
+    {
+        public Animal(string name) : this(name, 42)
+        {
+        }
+        public Animal(string name, int favoriteNumber)
+        {
+            Console.WriteLine("\nThe {0} is rare and beautiful, and its favorite number is {1}", name, favoriteNumber);
+        }
+    }
+}
---
+++ const_var_constructorchain/const_var_constructorchain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace const_var_constructorchain
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string helloWorld = "\nHello world, I'm a const!";
+            Console.WriteLine(helloWorld);
+
+            var person = 13;
+            Console.WriteLine("\n\nHi, I'm " + person + "! I've been implicitly defined as an int, but I'm called a 'person' in the code! \nI'm... not sure what that makes me, tbh \n");
+
+            // defines a new object using both possible parameters
+            Animal Camel = new Animal("Camel", 25699227);
+            // uses the chained constructor that only takes one of the parameters, giving it 42 as the second by default
+            Animal Zebra = new Animal("Zebra");
+
+            Console.Read();
+        }
+    }
+}
