a04f4eb64244868d5f3f398f7ebf4f600ff3500b
turtelneck <65428036+turtelneck@users.noreply.github.com>
2021-03-01 02:46:08 -0700

    Update logs

---
+++ enum_basics/enum_basics/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace enum_basics
+{
+    class Class1
+    {
+        public enum DaysOfTheWeek
+        {
+
+        }
+    }
+}
---
+++ operator_overload/operator_overload/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace operator_overload
+{
+    class Class1
+    {
+    }
+}
